8df7d713e5b00553bf3dc893f95e5305c8da2e80
Ivison Joel <ivison_j@outlook.com>
2025-02-17 18:14:42 -0300

    feat: implement JWT authentication with sign-in functionality and auth guard

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,6 +3,8 @@ import {
   Body,
   ConflictException,
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
   ValidationPipe,
 } from '@nestjs/common';
@@ -13,4 +15,10 @@ import { CreateUserAccountDTO } from './user.DTO';
 @Controller('api/v1/user')
 export class UserController {
   constructor(private readonly repo: UserService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.repo.signIn(signInDto.email);
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,22 @@
 import { v4 as uuid } from 'uuid';
-import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/shared/infra/database/prisma.service';
 import { CreateUserAccountDTO } from './user.DTO';
 import { UserErrors } from './user.errors';
 
+export interface SignInResponse {
+  access_token: string;
+  email: string;
+  isAdmin: boolean;
+}
+
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async createUser(user: CreateUserAccountDTO) {
     const newUser = await this.prisma.user.create({
@@ -17,5 +27,23 @@ export class UserService {
       },
     });
     return newUser;
+  }
+
+  async signIn(email: string): Promise<SignInResponse> {
+    const user = await this.prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) throw new NotFoundException('Usuário não encontrado');
+
+    const payload = { sub: user.id, email: user.email };
+
+    const access_token = await this.jwtService.signAsync(payload);
+
+    return {
+      access_token: access_token,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    };
   }
 }
