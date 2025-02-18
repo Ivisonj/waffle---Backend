f986ce583f50063f0aed7d6df3f3e29cda8ccec6
Ivison Joel <ivison_j@outlook.com>
2025-02-17 21:52:55 -0300

    feat: enhance newsletter creation with consecutive day tracking for user streaks

--- src/modules/newsletter/newsletter.service.ts
+++ src/modules/newsletter/newsletter.service.ts
@@ -8,17 +8,58 @@ export class NewsletterService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createNewsletter(newsletter: createNewsletterDTO) {
-    await this.prisma.newsletters.create({
+    const createNewsletter = await this.prisma.newsletters.create({
       data: {
         id: uuid(),
         resource_id: newsletter.resource_id,
         userId: newsletter.userId,
-        opened_at: new Date(),
+        opened_at: new Date('2025-02-13'),
         utm_source: newsletter.utm_source,
         utm_medium: newsletter.utm_medium,
         utm_campaign: newsletter.utm_campaign,
         utm_channel: newsletter.utm_channel,
       },
     });
+
+    if (createNewsletter) {
+      const findLastOpening = await this.prisma.newsletters.findFirst({
+        where: {
+          userId: newsletter.userId,
+          id: { not: createNewsletter.id },
+        },
+        select: {
+          opened_at: true,
+        },
+        orderBy: {
+          opened_at: 'desc',
+        },
+      });
+
+      const lastOpening = new Date(findLastOpening.opened_at);
+      const lastDay = lastOpening.getDay() + 1;
+      console.log('lastDay', lastDay);
+      const currentDate = new Date();
+      const currentDay = currentDate.getDay() + 1;
+      console.log('currentDay', currentDay);
+
+      const isConsecutiveDay = lastDay + 1 === currentDay;
+
+      if (isConsecutiveDay) {
+        await this.prisma.user.update({
+          where: { id: newsletter.userId },
+          data: {
+            current_streak: { increment: 1 },
+          },
+        });
+      }
+      if (!isConsecutiveDay) {
+        await this.prisma.user.update({
+          where: { id: newsletter.userId },
+          data: {
+            current_streak: 0,
+          },
+        });
+      }
+    }
   }
 }
